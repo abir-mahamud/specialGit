87b9dc14cbc6455f884673dfb1e109c809a18556
Abir Mahamud <41789784+abir-mahamud@users.noreply.github.com>
2019-09-15 01:38:16 +0600

    Variables & names exercise

--- Basic.py
+++ Basic.py
@@ -23,3 +23,19 @@ print("How about some more.")
 print("Is it greater?", 5 > -2)
 print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <= -2)
+
+
+
+#Variables &names
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available")
+print("We can transport", carpool_capacity, "people today")
+print("we need to put about", average_passengers_per_car, "in each car")
